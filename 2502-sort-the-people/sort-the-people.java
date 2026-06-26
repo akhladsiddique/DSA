396d438ea8e865ab60619b3b05306e9d391ce6f2
Md akhlad Hussain <103859829+akhladsiddique@users.noreply.github.com>
2026-06-27 01:44:26 +0530

    Time: 6 ms (99.08%) | Memory: 46.9 MB (95.02%) - LeetSync

--- 2502-sort-the-people/sort-the-people.java
+++ 2502-sort-the-people/sort-the-people.java
@@ -1,23 +1,30 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        int n1=names.length;
-        for(int i=0;i<n1;i++){
-            for(int  j=i+1;j<n1;j++){
-                if(heights[i]<heights[j])
-                {
-                    int temp1 = heights[i];
-                    heights[i] = heights[j];
-                    heights[j] = temp1;
-
-                    String temps = names[i];
-                        names[i] = names[j];
-                        names[j] = temps;
-
-                }
-
+        int n = heights.length;
+        
+        // 1. Find the maximum height to determine our bucket size
+        int maxHeight = 0;
+        for (int h : heights) {
+            if (h > maxHeight) {
+                maxHeight = h;
             }
         }
-        return names;
         
+        // 2. Create an array where the index represents the height
+        String[] heightToName = new String[maxHeight + 1];
+        for (int i = 0; i < n; i++) {
+            heightToName[heights[i]] = names[i];
+        }
+        
+        // 3. Collect names in descending order of heights
+        String[] result = new String[n];
+        int idx = 0;
+        for (int i = maxHeight; i >= 0; i--) {
+            if (heightToName[i] != null) {
+                result[idx++] = heightToName[i];
+            }
+        }
+        
+        return result;
     }
 }
