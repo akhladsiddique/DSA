c3486922fb325b00a29941538dec228a77aa2c7b
Md akhlad Hussain <103859829+akhladsiddique@users.noreply.github.com>
2026-07-09 00:48:07 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (51.41%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,24 +1,34 @@
 class Solution {
     public String toLowerCase(String s) {
         
-        StringBuilder sb = new StringBuilder(s);
-        int n=sb.length();
-        for(int i=0;i<n;i++){
-            char ch=sb.charAt(i);
-            int ascii=(int)ch;
-            if(ascii>=65 && ascii<=90){
-              ascii=ascii+32;
-              ch=(char)ascii;
-              sb.setCharAt(i,ch);
+        // StringBuilder sb = new StringBuilder(s);
+        // int n=sb.length();
+        // for(int i=0;i<n;i++){
+        //     char ch=sb.charAt(i);
+        //     int ascii=(int)ch;
+        //     if(ascii>=65 && ascii<=90){
+        //       ascii=ascii+32;
+        //       ch=(char)ascii;
+        //       sb.setCharAt(i,ch);
               
-            }
-            else{
-                ch=(char)ascii;
-                sb.setCharAt(i,ch);
+        //     }
+        //     else{
+        //         ch=(char)ascii;
+        //         sb.setCharAt(i,ch);
+        //     }
+        // }
+        // return sb.toString();
+
+
+        char[] arr = s.toCharArray();
+        for (int i=0;i<arr.length;i++){
+            if (arr[i]>='A' && arr[i]<='Z') {
+                arr[i]+= 32;
             }
         }
-        return sb.toString();
-        
+        return new String(arr);
+    
+
         
     }
 }
